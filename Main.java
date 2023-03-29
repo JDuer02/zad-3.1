ffc12330601176962743d12fbb22bd7cbea51fd3
JDuer02 <126687677+JDuer02@users.noreply.github.com>
2023-03-29 12:43:42 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+
+
+    String znak = "*";
+    for (int i=1; i<=4; i++)
+      {
+        for (int j=1; j<=4; j++)
+          System.out.print(znak);
+  
+    System.out.println(" ");}
+
+
+      }
+      
+  }
